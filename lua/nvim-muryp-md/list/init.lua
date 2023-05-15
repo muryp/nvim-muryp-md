c485d93aa7c966d0f0e376f9ea1daee57c6e2ede
alifprihantoro <alifmuryp@gmail.com>
2023-05-15 09:13:28 +0700

    feat: change sub and parent (comment)

--- lua/nvim-muryp-md/list/init.lua
+++ lua/nvim-muryp-md/list/init.lua
@@ -3,6 +3,66 @@ local M = {}
 local isCeklist = function(line)
   return string.find(line, "^%s*%-[ ]%[[%s|x]%]")
 end
+local cekIndent = function(line)
+
+end
+--- count how many cheked and unchecked in top of current
+local countTopList = function()
+  ---get table content from top to current_line
+  -- unchecked = 0 local parent_= 0
+  -- for i=current_line,#table (use reverse table or count form bottom)
+  -- if table[i] === regex_cheked
+  -- cheked = +1
+  -- local_parent = +1
+  -- if table[i] === regex_uncheked
+  -- local_parent = +1
+  -- else
+  -- break
+  -- end
+  -- if parent > 0 and parent level < current level
+  -- return {unchecked,line_parent}
+  -- else
+  -- false
+end
+local countBottomList = function()
+  ---get table content from current_line to bottom
+  -- for i=current_line,#table
+  -- local cheked = 0 , unchecked = 0
+  -- if table[i] === regex_cheked
+  -- cheked = 0
+  -- if table[i] === regex_uncheked
+  -- uncheked = 0
+  -- else
+  -- break
+  -- end
+  -- return unchecked
+end
+local cheklistChildern = function(current)
+  ---get table content from current_line to bottom
+  -- local level_current = level current()
+  -- for i=current_line,#table
+  -- local level = table[i] cek level
+  -- if table[i] === level > level_current
+  -- if unchecked
+  -- cheked(line)
+  -- if not checklist
+  -- break
+end
+local isSubCeklist = function(current)
+  cheklistChildern()
+  TOP = countTopList(current)
+  if TOP then
+    BOTTOM = countBottomList(current)
+  local COUNT_UNCHECK = BOTTOM + TOP.unchecked
+    --- if no uncheked
+    -- cheked parent
+    -- else
+    -- unchecked parent
+  end
+  --
+  -- if parent have parent
+  -- isSubCeklist(parent_line)
+end
 M.cheklist = function()
   local GET_CURRENT_LINE = api.nvim_get_current_line()
   local new_line
@@ -14,19 +74,6 @@ M.cheklist = function()
   if new_line then
     api.nvim_set_current_line(new_line)
   end
-end
-
-local function is_list_item(line)
-  return string.match(line, "^%s*[%-%*%+]?%s*[0-9]*%.?%s+.*$")
-end
-local function isListEmpty(line)
-  local delete_point = string.gsub(line, "^%s*[-*+]%s+", '', 1)
-  local delete_checkbox = string.gsub(delete_point, "^%[.%] ", '', 1)
-  local delete_number = string.gsub(delete_checkbox, "^%s*%d+%.%s+", '', 2)
-  if delete_number == '' then
-    return true
-  end
-  return false
 end
 
 local function cekLevel()
@@ -42,6 +89,18 @@ local function cekLevel()
   -- menghitung level indentasi
   local level = math.floor(indent_spaces / spaces)
   return level
+end
+local function is_list_item(line)
+  return string.match(line, "^%s*[%-%*%+]?%s*[0-9]*%.?%s+.*$")
+end
+local function isListEmpty(line)
+  local delete_point = string.gsub(line, "^%s*[-*+]%s+", '', 1)
+  local delete_checkbox = string.gsub(delete_point, "^%[.%] ", '', 1)
+  local delete_number = string.gsub(delete_checkbox, "^%s*%d+%.%s+", '', 2)
+  if delete_number == '' then
+    return true
+  end
+  return false
 end
 
 M.next_bullet = function()
