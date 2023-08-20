c19da366cc13ed4d9ea2f0d8a1500330d8f6b694
alifprihantoro <alifprihantoro@gmail.com>
2023-08-20 10:00:14 +0700

    fix: add setup plugins for link and delete not use

--- lua/nvim-muryp-md/init.lua
+++ lua/nvim-muryp-md/init.lua
@@ -1,11 +1,28 @@
-local ToggleCheck = require('nvim-muryp-md.list').toggleCheck
+local LINK_DEFAULT_CONFIGS = require('nvim-muryp-md.link.configs')
+local M = {}
 
-local DEFAULT_MAPS = {
-  ''
+M.configs = {
+  link = LINK_DEFAULT_CONFIGS
 }
-local DEFAULT_CMD = {
-  {
-    cmd = 'ToggleCheck',
-    func = ToggleCheck
-  }
-}
+M.setup = function(arg)
+  -- if arg.mapping ~= nil then
+  --   if arg.mapping.git ~= nil then
+  --     Setup.mapping.git = arg.mapping.git
+  --   end
+  --   if arg.mapping.issue ~= nil then
+  --     Setup.mapping.issue = arg.mapping.issue
+  --   end
+  -- end
+  -- if arg.SSH_PATH ~= nil then
+  --   Setup.SSH_PATH = arg.SSH_PATH
+  -- end
+  -- if arg.CACHE_DIR ~= nil then
+  --   Setup.CACHE_DIR = arg.CACHE_DIR
+  -- end
+  -- if arg.DEFAULT_REMOTE ~= nil then
+  --   Setup.DEFAULT_REMOTE = arg.DEFAULT_REMOTE
+  -- end
+  -- Setup.mapping()
+end
+
+return M
