c1d0366ba46676df983d627a8473c5643c6e03fe
alifprihantoro <alifprihantoro@gmail.com>
2023-08-20 09:54:56 +0700

    fix: split function for create link

--- lua/nvim-muryp-md/link/check.lua
+++ lua/nvim-muryp-md/link/check.lua
@@ -1,3 +1,4 @@
+local M = {}
 local isRawLink = function()
   local current_word = vim.fn.expand('<cWORD>') ---@type string
   local REGEX_RAW_LINK = "^https?://[%w-_%.%?%.:/%+=&]+"
@@ -8,10 +9,18 @@ local isRawLink = function()
   return false
 end
 
+local REGEX_MD = "%[(.-)%]%((.-)%)"
+local REGEX_WIKI = "%[%[(.-)%]%]"
+---get list link md/wiki
 ---@param CONTENT_LINE string
----@param CURRENT_COL number
----@param REGEX string
-local cekLinkMdWiki = function(CONTENT_LINE, CURRENT_COL, REGEX)
+---@param TYPE 'md'|'wiki'
+M.listLinkNum = function(CONTENT_LINE, TYPE)
+  local REGEX ---@type string - regex
+  if TYPE == 'md' then
+    REGEX = REGEX_MD
+  else
+    REGEX = REGEX_WIKI
+  end
   local startCol, endCol = CONTENT_LINE:find(REGEX)
   local LIST_LINK_NUM = {} ---@type {startCol:integer, endCol:integer}[]
   while startCol do
@@ -21,6 +30,14 @@ local cekLinkMdWiki = function(CONTENT_LINE, CURRENT_COL, REGEX)
     end
     startCol, endCol = CONTENT_LINE:find(REGEX, endCol + 1)
   end
+  return LIST_LINK_NUM
+end
+
+---@param CONTENT_LINE string
+---@param CURRENT_COL number
+---@param TYPE 'md'|'wiki
+local cekLinkMdWiki = function(CONTENT_LINE, CURRENT_COL, TYPE)
+  local LIST_LINK_NUM = M.listLinkNum(CONTENT_LINE, TYPE)
   if next(LIST_LINK_NUM) ~= nil then
     for _, VAL in pairs(LIST_LINK_NUM) do
       if VAL.startCol <= CURRENT_COL and VAL.endCol >= CURRENT_COL then
@@ -33,22 +50,20 @@ end
 local isLinkMdWiki = function()
   local CURRENT_COL = vim.fn.col('.') ---@type number
   local LINE_CONTENT = vim.api.nvim_get_current_line() ---@type string - get text on current line
-  local REGEX_MD = "%[(.-)%]%((.-)%)"
-  local LINK_MD = cekLinkMdWiki(LINE_CONTENT, CURRENT_COL, REGEX_MD)
+  local LINK_MD = cekLinkMdWiki(LINE_CONTENT, CURRENT_COL, 'md')
   if LINK_MD then
     local text, url = LINK_MD.CONTENT:match(REGEX_MD)
     return { text = text, url = url, isMdWiki = true, COL = LINK_MD.COL }
   end
-  local REGEX_WIKI = "%[%[(.-)%]%]"
-  local LINK_WIKI = cekLinkMdWiki(LINE_CONTENT, CURRENT_COL, REGEX_WIKI)
+  local LINK_WIKI = cekLinkMdWiki(LINE_CONTENT, CURRENT_COL, 'wiki')
   if LINK_WIKI then
-    local LINK_TEXT_WIKI = LINK_WIKI:match("%[%[(.-)%]%]")
+    local LINK_TEXT_WIKI = LINK_WIKI.CONTENT:match(REGEX_WIKI)
     return { text = LINK_TEXT_WIKI, url = LINK_TEXT_WIKI, isMdWiki = true, COL = LINK_MD.COL }
   end
   return false
 end
 
-local isLink = function()
+M.isLink = function()
   local MD_LINK = isLinkMdWiki()
   if MD_LINK then
     return MD_LINK
@@ -60,4 +75,4 @@ local isLink = function()
   return false
 end
 
-return isLink
+return M
