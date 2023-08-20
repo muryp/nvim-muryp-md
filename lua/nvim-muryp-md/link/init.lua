8f3f98bb7883887d4eb2743ad89ce42362c5b17f
alifprihantoro <alifprihantoro@gmail.com>
2023-08-20 09:58:01 +0700

    add new file and function in init

---
+++ lua/nvim-muryp-md/link/init.lua
@@ -0,0 +1,6 @@
+local M = {}
+M.check = require('nvim-muryp-md.link.check')
+M.configs = require('nvim-muryp-md.link.configs')
+M.create = require('nvim-muryp-md.link.create')
+M.unlink = require('nvim-muryp-md.link.unlink')
+return M
