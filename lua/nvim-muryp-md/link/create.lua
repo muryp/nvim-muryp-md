5f9b8da2529559caed5617997b453683fe925500
alifprihantoro <alifprihantoro@gmail.com>
2023-08-23 06:47:52 +0700

    fix: create link if select containt link

--- lua/nvim-muryp-md/link/create.lua
+++ lua/nvim-muryp-md/link/create.lua
@@ -4,30 +4,62 @@ local configs = require('nvim-muryp-md').configs
 
 local function replace_line(TEXT)
   local line_num = vim.fn.line('.')
-  local toLink = configs.link.replaceTexttoLink(TEXT)
+  local toLink = configs.link.replaceTexttoLink(TEXT.val)
   vim.api.nvim_buf_set_text(0, line_num - 1, TEXT.startCol - 1, line_num - 1, TEXT.endCol, { toLink })
 end
 
----@param LINE_CONTENT string
+---@param TABLE_LINK_MD_WIKI {startCol:integer, endCol:integer}[]|false
 ---@param startCol number
 ---@param endCol number
----@return boolean
-local cekNotConflictWithMdWiki = function(startCol, endCol, LINE_CONTENT)
-  local LIST_MD_LINK = checkLink.listLinkNum(LINE_CONTENT, 'md')
-  local LIST_WIKI_LINK = checkLink.listLinkNum(LINE_CONTENT, 'wiki')
-  local TABLE_MD_WIKI = mergeTable({ LIST_MD_LINK, LIST_WIKI_LINK }) ---@type {startCol:integer, endCol:integer}[]|false
-  if TABLE_MD_WIKI == false then
-    return false
+---@return {startCol:number,endCol:number}|nil
+local function cekNotConflictWithMdWiki(startCol, endCol, TABLE_LINK_MD_WIKI)
+  local CURRENT_LINE = vim.fn.col('.')
+  if TABLE_LINK_MD_WIKI == false then
+    return
   end
-  for _, value in pairs(TABLE_MD_WIKI) do
-    if value.endCol >= startCol then
-      return true
+  local CURRENT_LOOP = 1
+  ---loop list link and slice range
+  local function loopCek()
+    local LINK_MD_WIKI = TABLE_LINK_MD_WIKI[CURRENT_LOOP]
+    print('---table : ' .. CURRENT_LOOP)
+    CURRENT_LOOP = CURRENT_LOOP + 1
+    print(startCol, endCol)
+    if LINK_MD_WIKI == nil then
+      return
     end
-    if value.startCol <= endCol then
-      return true
+    local isTableOnRange = (LINK_MD_WIKI.startCol > startCol and LINK_MD_WIKI.startCol < endCol) or
+        (LINK_MD_WIKI.endCol > startCol and LINK_MD_WIKI.endCol < endCol)
+    local isOnFirst = LINK_MD_WIKI.startCol >= endCol
+
+    if isTableOnRange and isOnFirst then
+      print('---is first')
+      print(startCol, endCol)
+      endCol = LINK_MD_WIKI.startCol - 1
+      return
     end
+    local isOnSecond = LINK_MD_WIKI.endCol > startCol
+    if isTableOnRange and isOnSecond then
+      ---cek is current cursor in front of lastCol link
+      local isOnFrontLink = LINK_MD_WIKI.startCol > CURRENT_LINE
+      ---cek is current cursor in behind lastCol link
+      local isOnBackLink = LINK_MD_WIKI.endCol < CURRENT_LINE
+      if isOnFrontLink then
+        endCol = LINK_MD_WIKI.startCol - 1
+        print('---is frontlink')
+        print(startCol, endCol)
+        print(vim.inspect(LINK_MD_WIKI))
+        return loopCek()
+      end
+      if isOnBackLink then
+        startCol = LINK_MD_WIKI.endCol + 1
+        return loopCek()
+      end
+      return loopCek()
+    end
+    return loopCek()
   end
-  return false
+  loopCek()
+  return { startCol = startCol, endCol = endCol }
 end
 
 local getText = function()
@@ -57,12 +89,21 @@ local getText = function()
     end
     CURRENT_COL = NEXT_COL
   end
-  local isConflict = cekNotConflictWithMdWiki(startCol, endCol, LINE_CONTENT)
-  if isConflict == true then
+  local LIST_MD_LINK = checkLink.listLinkNum(LINE_CONTENT, 'md')
+  local LIST_WIKI_LINK = checkLink.listLinkNum(LINE_CONTENT, 'wiki')
+  local TABLE_MD_WIKI = mergeTable({ LIST_MD_LINK, LIST_WIKI_LINK }) ---@type {startCol:integer, endCol:integer}[]|false
+  local isConflict = cekNotConflictWithMdWiki(startCol, endCol, TABLE_MD_WIKI)
+  if isConflict then
+    startCol = isConflict.startCol
+    endCol = isConflict.endCol
+    local val = LINE_CONTENT:sub(startCol, endCol)
+    if val then
+      local result = { startCol = startCol, endCol = endCol, val = val }
+      print(vim.inspect(result))
+      return result
+    end
     return
   end
-  local val = LINE_CONTENT:sub(startCol, endCol)
-  return { startCol = startCol, endCol = endCol, val = val }
 end
 
 local function createLink()
